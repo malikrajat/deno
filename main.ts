62aedb4534f562bcb4148f77bb5fffb4c4bc2d64
er.rajatmalik <er.rajatmalik@gmail.com>
2020-06-14 00:14:55 +0530

    update main file with 404

--- main.ts
+++ main.ts
@@ -1,12 +1,11 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
 import router from "./routes.ts";
+import notFound from "./404.ts";
 
 const app = new Application();
 app.use(router.routes());
 
-app.use((ctx) => {
-  ctx.response.body = "Hello World!";
-});
+app.use(notFound);
 
 console.log("server is started at 8000");
 await app.listen({ port: 8000 });
