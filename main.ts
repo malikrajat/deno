df98baaf1b1ee5d761d9dfd4791b7637a41912d0
er.rajatmalik <er.rajatmalik@gmail.com>
2020-06-14 00:03:44 +0530

    slice route in to new file

--- main.ts
+++ main.ts
@@ -1,16 +1,5 @@
-import { Application, Router } from "https://deno.land/x/oak/mod.ts";
-
-const router = new Router();
-
-router.get("/user", (ctx) => {
-  ctx.response.body = "Hello Rajat,  You are a confiden person";
-})
-  .post("/user", async (ctx) => {
-    console.log(ctx.request.body());
-    const { value } = await ctx.request.body();
-    ctx.response.status = 201;
-    ctx.response.body = value;
-  });
+import { Application } from "https://deno.land/x/oak/mod.ts";
+import router from "./routes.ts";
 
 const app = new Application();
 app.use(router.routes());
