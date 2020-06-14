9918c5064c90ef491be4a550b1f18de5c24c3059
er.rajatmalik <er.rajatmalik@gmail.com>
2020-06-14 15:30:23 +0530

    commneting and updat routing

--- routes.ts
+++ routes.ts
@@ -1,10 +1,13 @@
+// importing the oak from the url.
 import { Router } from "https://deno.land/x/oak/mod.ts";
 import UserController from "./controller/UserController.ts";
 
+// create new instance
 const router = new Router();
 
+// ROUTES
 router
-  .get("/user", UserController.index)
+  .get("/", UserController.index)
   .post("/user", UserController.store)
   .get("/user/:id", UserController.show)
   .delete("/user/:id", UserController.delete)
