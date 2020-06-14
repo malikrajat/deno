ac26d061de4d2f738332a0cc9224fe41d2b5f528
er.rajatmalik <er.rajatmalik@gmail.com>
2020-06-14 09:04:40 +0530

    update route

--- routes.ts
+++ routes.ts
@@ -1,16 +1,13 @@
 import { Router } from "https://deno.land/x/oak/mod.ts";
+import UserController from "./controller/UserController.ts";
 
 const router = new Router();
 
-router.get("/user", (ctx) => {
-  ctx.response.body = "Hello Rajat,  You are a confiden person";
-})
-  .post("/user", async (ctx) => {
-    console.log(ctx.request.body());
-    const { value } = await ctx.request.body();
-    ctx.response.status = 201;
-    ctx.response.body = value;
-  }).get("/user/:id", (ctx) => {
-    ctx.response.body = ctx.params.id;
-  });
+router
+  .get("/user", UserController.index)
+  .post("/user", UserController.store)
+  .get("/user/:id", UserController.show)
+  .delete("/user/:id", UserController.delete)
+  .patch("/user/:id", UserController.update);
+
 export default router;
