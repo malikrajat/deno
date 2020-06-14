42a32f9fac1fb2c69f19038c4e6a30e7d211bf16
er.rajatmalik <er.rajatmalik@gmail.com>
2020-06-14 21:56:24 +0530

    update validation with get all error in one time

--- validation.ts
+++ validation.ts
@@ -2,20 +2,21 @@ import { trim } from "https://deno.land/x/dotenv/util.ts";
 
 export default {
   async Validate(ctx: any) {
+    let errors: any = [];
+    let status;
     let body: any = await ctx.request.body();
     const { value } = body;
     const fields = ["name", "email", "password", "avatar", "date"];
     for (let index = 0; index < fields.length; index++) {
       const element = value[fields[index]];
       if (!element || trim(element).length < 1) {
-        ctx.response.status = 422;
-        ctx.response.body = {
-          error: {
-            message: `${fields[index]} is required `,
-          },
-        };
-        return false;
+        errors.push({ [fields[index]]: `${fields[index]} is required ` });
+        status = 422;
       }
+    }
+    if (status) {
+      ctx.response.body = { errors };
+      return false;
     }
     return true;
   },
