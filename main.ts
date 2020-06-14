193eced3eefd50cec17b52555771c7d64d50d12c
er.rajatmalik <er.rajatmalik@gmail.com>
2020-06-14 15:30:07 +0530

    commneting and allow mwthod

--- main.ts
+++ main.ts
@@ -1,13 +1,20 @@
+// importing the oak from the url.
 import { Application } from "https://deno.land/x/oak/mod.ts";
 import { config } from "https://deno.land/x/dotenv/mod.ts";
+
+// importing the router
 import router from "./routes.ts";
+
+// importing the controllers
 import notFound from "./404.ts";
 
 const env = config();
 const app = new Application();
 app.use(router.routes());
-
+app.use(router.allowedMethods());
 app.use(notFound);
+
+// Starting the server
 const HOST = env.APP_HOST || "http://localhost:";
 const PORT = +env.APP_PORT || 4000;
 console.log(`server is started at ${HOST} ${PORT}`);
