1a4ea01b34636f8cdc69b1ac9c55c27049d53409
er.rajatmalik <er.rajatmalik@gmail.com>
2020-06-14 08:02:22 +0530

    update server file as per env file

--- main.ts
+++ main.ts
@@ -1,11 +1,14 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
+import { config } from "https://deno.land/x/dotenv/mod.ts";
 import router from "./routes.ts";
 import notFound from "./404.ts";
 
+const env = config();
 const app = new Application();
 app.use(router.routes());
 
 app.use(notFound);
-
-console.log("server is started at 8000");
-await app.listen({ port: 8000 });
+const HOST = env.APP_HOST || "http://localhost:";
+const PORT = +env.APP_PORT || 4000;
+console.log(`server is started at ${HOST} ${PORT}`);
+await app.listen({ port: PORT });
