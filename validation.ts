963bbb618dd45f8341740a84e7a26c703bab12c5
er.rajatmalik <er.rajatmalik@gmail.com>
2020-06-15 15:28:58 +0530

    login validation added

--- validation.ts
+++ validation.ts
@@ -20,4 +20,27 @@ export default {
     }
     return true;
   },
+
+  async LoginValidate(ctx: any) {
+    let errors: any = [];
+    let status;
+    const { value } = await ctx.request.body();
+    if (!value) {
+      ctx.response.status = 400; // bad request
+      ctx.response.body = { error: "Please provide required details." };
+    }
+    const fields = ["email", "password"];
+    for (let index = 0; index < fields.length; index++) {
+      const element = value[fields[index]];
+      if (!element || trim(element).length < 1) {
+        errors.push({ [fields[index]]: `${fields[index]} is required ` });
+        status = 422;
+      }
+    }
+    if (status) {
+      ctx.response.body = { errors };
+      return false;
+    }
+    return value;
+  },
 };
