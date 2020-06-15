a71245914bfb20bbc692df01882fbcd57b218e29
er.rajatmalik <er.rajatmalik@gmail.com>
2020-06-15 15:28:49 +0530

    login route added

--- routes.ts
+++ routes.ts
@@ -1,6 +1,7 @@
 // importing the oak from the url.
 import { Router } from "https://deno.land/x/oak/mod.ts";
 import UserController from "./controller/UserController.ts";
+import AuthController from "./controller/AuthController.ts";
 
 // create new instance
 const router = new Router();
@@ -12,5 +13,8 @@ router
   .get("/user/:id", UserController.show)
   .delete("/user/:id", UserController.delete)
   .patch("/user/:id", UserController.update);
+
+// login routes
+router.post("/login", AuthController.login);
 
 export default router;
